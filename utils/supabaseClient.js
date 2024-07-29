ad990846ba99e8f5832f338f9ea976af2644ea90
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-28 19:34:55 -0600

    Create supabaseClient.js

---
+++ utils/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://qflhgztoxamhdhlxjpjc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmbGhnenRveGFtaGRobHhqcGpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4Mzg5MTksImV4cCI6MjAzNjQxNDkxOX0.BWDSmZFBrQVSZV0wK6XWGaAZky51VxrRG_wfwG3FWZc';
+//const supabaseSecret = 'your-supabase-secret';
+
+export const supabase = createClient(supabaseUrl, supabaseKey); //supabaseSecret);
