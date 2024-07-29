15d696d1fdcc2c2342663ed24af62032336c87d1
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-28 20:34:32 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['your-supabase-instance.supabase.co'],
+    domains: ['https://qflhgztoxamhdhlxjpjc.supabase.co'],
   },
 };
