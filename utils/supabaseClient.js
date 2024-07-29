8067ca8042bd3cade7cb84354e0541c1c3fd1dbf
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-28 20:37:42 -0600

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qflhgztoxamhdhlxjpjc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmbGhnenRveGFtaGRobHhqcGpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA4Mzg5MTksImV4cCI6MjAzNjQxNDkxOX0.BWDSmZFBrQVSZV0wK6XWGaAZky51VxrRG_wfwG3FWZc';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
