15e89b15355df7d22ccc9018c6a8ad24538e6cca
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-30 21:16:43 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,64 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import DrinkCard from '../components/DrinkCard';
+import styles from '../styles/Home.module.css';
 
 function Home() {
   const [drink, setDrink] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRandomDrink = async () => {
+    try {
+      const res = await fetch('/api/supabase');
+      const data = await res.json();
+      setDrink(data); // assuming data is a single drink object
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleSearch = async (query) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/supabase?query=${query}`);
+      const data = await res.json();
+      setDrink(data[0]); // assuming data is an array of drinks
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleIngredientSearch = async (ingredient) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`/api/supabase?ingredient=${ingredient}`);
+      const data = await res.json();
+      setDrink(data[0]); // assuming data is an array of drinks
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetch('/api/supabase')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('Data from API:', data);
-        setDrink(data);
-      })
-      .catch((error) => console.error('Error fetching data:', error));
+    fetchRandomDrink();
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
-      <SearchBar />
-      {drink ? (
+      <SearchBar onSearch={handleSearch} onIngredientSearch={handleIngredientSearch} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : drink ? (
         <DrinkCard drink={drink} />
       ) : (
-        <p>Loading...</p>
+        <p>No drinks found.</p>
       )}
     </div>
   );
