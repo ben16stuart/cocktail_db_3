129d449019fac510088044105275c563e4e9563b
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-07-31 08:24:04 -0600

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -4,6 +4,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
+  console.error('Supabase URL or Key is missing!');
   throw new Error('Missing Supabase URL or Key. Check your environment variables.');
 }
 
