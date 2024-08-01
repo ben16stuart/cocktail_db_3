2eb45d73b0096b4733cb87be627b72ff154c6ceb
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-08-01 11:47:41 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
+// components/Header.js
 import React from 'react';
+import styles from '../styles/Header.module.css'; // Adjust the path if needed
 
 const Header = () => {
   return (
-    <header>
-      <h1>Cocktail Recipe Book</h1>
-      <img src="/logo.jpeg" alt="Logo" className="logo" />
+    <header className={styles.header}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Cocktail Database</h1>
+        <img src="/logo.png" alt="Logo" className={styles.logo} />
+      </div>
     </header>
   );
 };
