1f2f3b292192ca4bf08c0712d18e1d507a2d54eb
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-08-01 09:40:56 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import DrinkCard from '../components/DrinkCard';
-import '../styles/globals.css';
+import '../styles/global.css';
 
 function Home() {
   const [drink, setDrink] = useState(null);
