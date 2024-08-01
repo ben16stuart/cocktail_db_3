e230fa1811127c25b9674bc567c5f107f0aac63f
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-08-01 09:30:46 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import '../styles/globals.css'; 
+import '../styles/globals.css'; // Ensure global styles are available
 
-function Header() {
+const Header = () => {
   return (
     <header>
-      <h1>Cocktail Recipe Book</h1>
+      <img src="/logo.png" alt="Logo" className="logo" />
     </header>
   );
-}
-
-export default Header;
-
-
-//import React from 'react';
-//import '../styles/globals.css'; // Ensure global styles are available
-
-//const Header = () => {
-//  return (
-//    <header>
-//      <img src="/logo.png" alt="Logo" className="logo" />
-//    </header>
-//  );
-//};
+};
