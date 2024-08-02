f71f5346c6879b52f2681d5ca736cf66be945ed5
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-08-02 09:16:52 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-ximport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import SearchBar from '../components/SearchBar';
 import DrinkCard from '../components/DrinkCard';
