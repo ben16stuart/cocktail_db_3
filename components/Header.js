2f499160d0e6d6518eaf1fd8bbee53c13e8b967e
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-09-04 20:21:54 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
-// components/Header.js
 import React from 'react';
 import styles from '../styles/Header.module.css'; // Import the correct CSS module
 
 const Header = () => {
+  const reloadPage = () => {
+    window.location.reload(); // Reloads the current page
+  };
+
   return (
     <header className={styles.headerContainer}>
-      <h1>Cocktail Database</h1>
-      <img src="/logo.png" alt="Logo" className={styles.logo} />
+      <h1 onClick={reloadPage} className={styles.clickableText}>Cocktail Database</h1>
+      <img src="/logo.png" alt="Logo" className={styles.logo} onClick={reloadPage} />
     </header>
   );
 };
