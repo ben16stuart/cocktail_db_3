7a4d7899a3e4ba6a5139bcec6ad3e228099133d0
Ben <104168127+ben16stuart@users.noreply.github.com>
2024-12-17 19:11:31 -0700

    Update DrinksByName.js

--- pages/api/DrinksByName.js
+++ pages/api/DrinksByName.js
@@ -37,7 +37,8 @@ export default async function handler(req, res) {
     const { data: ingredients, error: ingredientsError } = await supabase
       .from('ingredients_v')
       .select('id, name')
-      .in('id', ingredientIds);
+      .in('id', ingredientIds)
+      .order('i_t_id', { ascending: true });
 
     if (ingredientsError) {
       throw ingredientsError;
