42283490da7519be5debb61c7610476c112ace39
Ben <104168127+ben16stuart@users.noreply.github.com>
2025-03-16 21:55:06 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -10,6 +10,17 @@ const Header = () => {
     <header className={styles.headerContainer}>
       <h1 onClick={reloadPage} className={styles.clickableText}>Cocktail Database</h1>
       <img src="/logo.png" alt="Logo" className={styles.logo} onClick={reloadPage} />
+      
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+
+      <!-- Apple Touch Icon -->
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+
+      <!-- Android/Chrome -->
+      <link rel="manifest" href="/site.webmanifest">
+
+  
     </header>
   );
 };
