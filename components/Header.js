0bf0aa36c4e2c6d64f94f0eac423bb64c87bb019
Ben <104168127+ben16stuart@users.noreply.github.com>
2025-03-16 22:01:58 -0600

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -14,10 +14,7 @@ const Header = () => {
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 
-      //<!-- Apple Touch Icon -->
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-
-      //<!-- Android/Chrome -->
       <link rel="manifest" href="/site.webmanifest">
 
   
